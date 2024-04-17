f397a788d1424ed0b489c988a5b5f10fd7bbf5d7
begona-paella <begona@paella.dev>
2024-04-17 16:34:11 -0500

    get nfts from wallet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import React, { useState } from "react";
 import { createWallet } from "./signup/actions";
 import { mintNFT } from "./export/mint";
+import { getNFTFromWallet } from './nfts/get-nft';
 
 export default function Home() {
   const [email, setEmail] = useState("");
@@ -16,6 +17,12 @@ export default function Home() {
     mintNFT(email, imageUrl);
   };
 
+  const handleGetNFTFromNFT = () => {
+    // this needs to be dynamic but with this structure: polygon:walletId
+    getNFTFromWallet("polygon:0x970c4f6EcDecada4875744e3E52154cB208Fe508");
+  }
+
+  
   return (
     <div className="flex h-full flex-col items-center justify-center gap-20">
       <div className="flex flex-col text-center gap-2 mt-[45%] md:mt-[15%]">
@@ -29,9 +36,8 @@ export default function Home() {
           className="mt-4"
         />
         <button onClick={handleCreateWallet}>Create Wallet</button>
-        <button onClick={handleMintNFT} className="mt-4">
-          Mint NFT
-        </button>
+        <button onClick={handleMintNFT} className="mt-4">Mint NFT</button>
+        <button onClick={handleGetNFTFromNFT} className="mt-4">Get NFT from wallet</button>
       </div>
     </div>
   );
