fdf4df4a6a642be053042fc8c4cff53742264be8
begona-paella <begona@paella.dev>
2024-04-17 12:38:30 -0500

    add sign action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,36 @@
-"use client";
+"use client";  
 
+import React, { useState } from 'react';
+import { createWallet } from './signup/actions'; 
 export default function Home() {
-  const openCamara = () => {
+  const [email, setEmail] = useState('');
+
+  const openCamera = () => {
     navigator.mediaDevices.getUserMedia({
       video: true,
+    }).then(stream => {
+    }).catch(err => {
+      console.error("Error accessing the camera: ", err);
     });
   };
+
+  const handleCreateWallet = () => {
+    createWallet(email);
+  };
+
   return (
-    <div className="flex h-full flex-col items-center justify-center gap-40">
+    <div className="flex h-full flex-col items-center justify-center gap-20">
       <div className="flex flex-col text-center gap-2 mt-[45%] md:mt-[15%]">
         <h1>Photo Verifier</h1>
-        <button onClick={openCamara}>Take Photo</button>
+        <button onClick={openCamera}>Take Photo</button>
+        <input
+          type="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          placeholder="Enter your email"
+          className="mt-4"
+        />
+        <button onClick={handleCreateWallet}>Create Wallet</button>
       </div>
     </div>
   );
